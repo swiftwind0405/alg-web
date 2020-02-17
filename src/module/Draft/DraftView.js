bc1fd316ab36c41901d5981133db91ba1e4ea2df
Stanley.Wind <swiftwind0405@gmail.com>
2020-02-17 17:22:42 +0800

    init

--- src/module/Draft/DraftView.js
+++ src/module/Draft/DraftView.js
@@ -3,7 +3,7 @@ import ss from './DraftView.less';
 function DraftView() {
     return (
         <div className={ss.wrapper}>
-            <h1 className={ss.quote}>Coming soon......</h1>
+            <h1 className={ss.quote}>Coming soon...</h1>
             <div className={ss.author}>—— Jet & Stanley</div>
         </div>
     );
